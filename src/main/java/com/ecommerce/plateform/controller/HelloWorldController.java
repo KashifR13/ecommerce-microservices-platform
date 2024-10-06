dfe457b3096890dc976d7e4540d868f32cb19f37
Kashif <kashif.rasool@tuni.fi>
2024-10-07 02:50:20 +0300

    Project kickstarted

---
+++ src/main/java/com/ecommerce/plateform/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.ecommerce.plateform.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
