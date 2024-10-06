b1863128b9abf1a2ef6eeae96934f8da449f296f
Kashif <kashif.rasool@tuni.fi>
2024-10-07 01:58:15 +0300

    Refactored classes; spells corrected

--- src/main/java/com/ecommerce/plateform/PlatformApplication.java
+++ src/main/java/com/ecommerce/plateform/PlatformApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlateformApplication {
+public class PlatformApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlateformApplication.class, args);
+		SpringApplication.run(PlatformApplication.class, args);
 	}
 
 }
