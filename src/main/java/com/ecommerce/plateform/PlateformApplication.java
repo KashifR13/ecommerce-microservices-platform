dcee38d1082e80fc07590c15814f4df44d76dea6
Kashif <kashif.rasool@tuni.fi>
2024-10-07 01:56:22 +0300

    Initial project setup; dependencies added

---
+++ src/main/java/com/ecommerce/plateform/PlateformApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.plateform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/plateform/PlateformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.plateform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
